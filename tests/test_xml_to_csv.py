732710921f5d63fef46fd5d0c9c65622d16d3197
SpiralOutDotEu <spiralout.eu@gmail.com>
2019-11-24 18:59:01 +0200

    reformat code

--- tests/test_xml_to_csv.py
+++ tests/test_xml_to_csv.py
@@ -2,19 +2,19 @@ from unittest.mock import patch, MagicMock
 import unittest
 from unittest import mock
 import cookiepy as cp
-import xml.etree.ElementTree as ET
 
 
-class mock_tree():
-    def getroot(self):
+class MockTree:
+    @staticmethod
+    def getroot():
         return "getroot"
 
 
 def mock_parse(self):
-    return mock_tree()
+    return MockTree()
 
 
-class TestXML_to_CSV(unittest.TestCase):
+class TestItConvertsXMLtoCSV(unittest.TestCase):
 
     @mock.patch('xml.etree.ElementTree.parse', side_effect=mock_parse)
     def test_it_converts_XML_to_element(self, mock_parse):
