b4edc7d1f49642d8a82a1967546c5d95b5a492fa
isykasle <55947653+isykasle@users.noreply.github.com>
2020-02-09 22:08:58 +0200

    Update __init__.py
    Import of csv_to_tfrecord funtion from generate_tfrecords.py module

--- cookiepy/__init__.py
+++ cookiepy/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from .files import *
 from .xml_to_csv import *
+from .generate_tfrecords import  .csv_to_tfrecord
 """Top-level package for cookiepy."""
 
 __author__ = """Nikos Koumbakis"""
