0e0e9c293b9862e0f3dc10cff71a66d20c00cc4a
SpiralOutDotEu <spiralout.eu@gmail.com>
2020-09-21 21:18:38 +0300

    Use of object patch instead of method patch to pass from Travis too.

--- tests/test_tf_models.py
+++ tests/test_tf_models.py
@@ -1,15 +1,16 @@
 import unittest
 from cookiepy import tf_models
 from unittest import mock
+from dulwich import porcelain
 
 url = "https://github.com/tensorflow/models.git"
 default_path = "data/models"
 
 
-class TestModelZoo(unittest.TestCase):
+class TestTfModels(unittest.TestCase):
 
     @mock.patch('os.mkdir', autospec=True)
-    @mock.patch('tf_models.porcelain.clone', autospec=True)
+    @mock.patch.object(porcelain, 'clone', autospec=True)
     def test_it_clones_models_to_data_models_by_default(self, mock_git_clone, mock_mkdir):
         tf_models.clone_models()
 
@@ -17,7 +18,7 @@ class TestModelZoo(unittest.TestCase):
         mock_git_clone.assert_called_with(url, default_path)
 
     @mock.patch('os.mkdir', autospec=True)
-    @mock.patch('tf_models.porcelain.clone', autospec=True)
+    @mock.patch.object(porcelain, 'clone', autospec=True)
     def test_it_clones_models_to_specified_folder(self, mock_git_clone, mock_mkdir):
         tf_models.clone_models(models_folder='zoo/folder')
 
