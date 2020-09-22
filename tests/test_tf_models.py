d57118014c2da116e3e808366bf512d2a0d0b866
SpiralOutDotEu <spiralout.eu@gmail.com>
2020-09-22 19:42:54 +0300

    fixed path exists error

--- tests/test_tf_models.py
+++ tests/test_tf_models.py
@@ -9,18 +9,18 @@ default_path = "data/models"
 
 class TestTfModels(unittest.TestCase):
 
-    @mock.patch('os.mkdir', autospec=True)
+    @mock.patch('os.makedirs', autospec=True)
     @mock.patch.object(porcelain, 'clone', autospec=True)
     def test_it_clones_models_to_data_models_by_default(self, mock_git_clone, mock_mkdir):
         tf_models.clone_models()
 
-        mock_mkdir.assert_called_with("data/models")
+        mock_mkdir.assert_called_with("data/models", exist_ok=True)
         mock_git_clone.assert_called_with(url, default_path)
 
-    @mock.patch('os.mkdir', autospec=True)
+    @mock.patch('os.makedirs', autospec=True, exist_ok=True)
     @mock.patch.object(porcelain, 'clone', autospec=True)
     def test_it_clones_models_to_specified_folder(self, mock_git_clone, mock_mkdir):
         tf_models.clone_models(models_folder='zoo/folder')
 
-        mock_mkdir.assert_called_with("zoo/folder")
+        mock_mkdir.assert_called_with("zoo/folder", exist_ok=True)
         mock_git_clone.assert_called_with(url, "zoo/folder")
