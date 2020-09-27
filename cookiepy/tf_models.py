5f455e90cd38ac259c77c0105771e3f8d07b8050
SpiralOutDotEu <spiralout.eu@gmail.com>
2020-09-27 23:01:02 +0300

    it install object detection api from models repo

--- cookiepy/tf_models.py
+++ cookiepy/tf_models.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from dulwich import porcelain
 
 url = "https://github.com/tensorflow/models.git"
@@ -10,3 +11,16 @@ def get_models(models_folder=path, models_url=url):
     os.makedirs(models_folder, exist_ok=True)
     porcelain.clone(models_url, models_folder)
     return True
+
+
+def install_object_detection(models_path='models'):
+    subprocess.run(get_install_script(models_path))
+    return True
+
+
+def get_install_script(models_path='models'):
+    cd_to_research = "cd {}/research/; ".format(models_path)
+    proto_build = "protoc object_detection/protos/*.proto --python_out=.; "
+    copy_setup = "cp object_detection/packages/tf2/setup.py .; "
+    pip_install = "python -m pip install .; "
+    return cd_to_research + proto_build + copy_setup + pip_install
--- tests/test_tf_models.py
+++ tests/test_tf_models.py
@@ -2,9 +2,18 @@ import unittest
 from cookiepy import tf_models
 from unittest import mock
 from dulwich import porcelain
-
+import subprocess
 url = "https://github.com/tensorflow/models.git"
 default_path = "models"
+
+cd_to_research = "cd models/research/; "
+proto_build = "protoc object_detection/protos/*.proto --python_out=.; "
+copy_setup = "cp object_detection/packages/tf2/setup.py .; "
+pip_install = "python -m pip install .; "
+install_script = cd_to_research + proto_build + copy_setup + pip_install
+
+cd_to_research_custom_models_folder = "cd my_folder/my_model/research/; "
+custom_install_script = cd_to_research_custom_models_folder + proto_build + copy_setup + pip_install
 
 
 class TestTfModels(unittest.TestCase):
@@ -24,3 +33,26 @@ class TestTfModels(unittest.TestCase):
 
         mock_mkdir.assert_called_with("zoo/folder", exist_ok=True)
         mock_git_clone.assert_called_with(url, "zoo/folder")
+
+    def test_it_returns_the_object_detection_install_script(self):
+        assert tf_models.get_install_script() == install_script
+
+    def test_it_returns_the_object_detection_install_script_from_another_model_folder(self):
+        assert tf_models.get_install_script('my_folder/my_model') == custom_install_script
+
+    @mock.patch.object(subprocess, 'run', autospec=True)
+    def test_it_run_object_detection_api_from_default_folder(self, sub_process_run):
+        tf_models.install_object_detection()
+        sub_process_run.assert_called_with(install_script)
+
+    @mock.patch.object(tf_models, 'get_install_script', autospec=True)
+    @mock.patch.object(subprocess, 'run', autospec=True)
+    def test_it_run_object_detection_api_from_another_folder(self, sub_process_run, get_install_path):
+        some_folder_path = 'folder1/folder2'
+        get_install_path.return_value = 'patch'
+
+        tf_models.install_object_detection(models_path=some_folder_path)
+
+        get_install_path.assert_called_with(some_folder_path)
+        sub_process_run.assert_called_with('patch')
+
